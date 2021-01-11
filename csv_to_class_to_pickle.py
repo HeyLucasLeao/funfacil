8eb9b6a1e32401d5b5bc521167e132c0290046cf
Lucas Leão <ilolt.llol@gmail.com>
2021-01-11 16:23:18 -0400

    Create csv_to_class_to_pickle.py

---
+++ csv_to_class_to_pickle.py
@@ -0,0 +1,36 @@
+import csv
+import pickle
+
+from dataclasses import dataclass
+
+
+@dataclass
+class GenericClass:
+    id: str
+    name: str
+    email: str
+
+
+def csv_to_class(path, encoding):
+    arr = []
+
+    with open(path, encoding=encoding) as file:
+        to_read = csv.reader(file)
+        for line in to_read:
+            id, name, email = line
+
+            line = GenericClass(id, name, email)
+            arr.append(line)
+    return arr
+
+
+def class_to_pickle(arr, path):
+    with open(path, mode='wb') as file:
+        pickle.dump(arr, file)
+    return f'the file has been pickled. You can look at the PATH: ({path})'
+
+
+def pickles_to_class(path):
+    with open(path, mode='rb') as file:
+        data = pickle.load(file)
+    return data
