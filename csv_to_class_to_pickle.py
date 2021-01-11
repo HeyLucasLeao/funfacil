9601ed4dc9496527f5cf2b21a64872aee70d489e
Lucas Leão <ilolt.llol@gmail.com>
2021-01-11 16:39:31 -0400

    Update csv_to_class_to_pickle.py

--- csv_to_class_to_pickle.py
+++ csv_to_class_to_pickle.py
@@ -1,5 +1,6 @@
 import csv
 import pickle
+import json
 
 from dataclasses import dataclass
 
@@ -24,7 +25,7 @@ def csv_to_class(path, encoding):
     return arr
 
 
-def class_to_pickle(arr, path):
+def obj_to_pickle(arr, path):
     with open(path, mode='wb') as file:
         pickle.dump(arr, file)
     return f'the file has been pickled. You can look at the PATH: ({path})'
@@ -34,3 +35,17 @@ def pickle_to_class(path):
     with open(path, mode='rb') as file:
         data = pickle.load(file)
     return data
+
+
+def obj_to_json(obj, path):
+    with open(path, mode='w') as file:
+        json.dump(obj, file, default=_dict_to_json)
+
+
+def _dict_to_json(obj):
+    obj.__dict__
+
+
+def json_to_obj(path):
+    with open(path) as file:
+        arr_dict = json.load(file)
