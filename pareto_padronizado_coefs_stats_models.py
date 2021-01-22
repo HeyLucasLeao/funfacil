2fb5d866e2161e36ebce231aba3d56d193ed9381
Lucas Leão <ilolt.llol@gmail.com>
2021-01-22 13:43:16 -0400

    Update and rename visualizar_coefs_stats_models.py to pareto_padronizado_coefs_stats_models.py

--- pareto_padronizado_coefs_stats_models.py
+++ pareto_padronizado_coefs_stats_models.py
@@ -1,10 +1,10 @@
-def visualizar_modelo_treinado(model, alpha=0.05, x=None, y=None):
+def visualizar_modelo_treinado(model, alpha=0.05):
     from scipy import stats
     distribuicao = stats.t(df = model.df_resid)
     t_values = model.tvalues
     df = t_values.to_frame(name='coef').reset_index()
     df.rename(columns={'index': 'variaveis'}, inplace=True)
-    limite = distribuicao.ppf(q= 1 - (alpha/2))*len(df)
-    fig = px.bar(df.sort_values('coef', ascending=True), x=x, y=y)
+    limite = distribuicao.ppf(q= 1 - alpha/2)
+    fig = px.bar(df.sort_values('coef', ascending=True), x='coef', y='variaveis')
     fig.add_vline(x= limite, line_color='red')
     return fig
