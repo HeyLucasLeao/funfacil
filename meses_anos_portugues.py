510801bca98c3fbcf937765a5bde5edff347d94a
Lucas Leão <ilolt.llol@gmail.com>
2021-01-29 16:00:08 -0400

    Update meses_anos_portugues.py

--- meses_anos_portugues.py
+++ meses_anos_portugues.py
@@ -1,4 +1,4 @@
-def meses_anos():
+def meses_anos(data):
     from calendar import month_abbr
     import datetime
     import numpy as np
@@ -14,11 +14,10 @@ def meses_anos():
         'Sep': 'Set',
         'Oct': 'Out', 
         'Nov': 'Nov',
-            
         'Dec': 'Dez'}
     dois_meses_seguintes = str(datetime.datetime.now() + datetime.timedelta(weeks=8))[:10]
     
-    dias = np.arange('2020-04-01', dois_meses_seguintes, dtype='datetime64[M]')
+    dias = np.arange(data, dois_meses_seguintes, dtype='datetime64[M]')
     anos_sigla = [str(x)[:4] for x in dias]
     mes_int = [int(str(x)[5:7]) for x in dias]
     mes_sigla_ingles = [month_abbr[x] for x in mes_int]
