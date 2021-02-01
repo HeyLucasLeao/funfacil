3747b9b8e76661678d3d4b813f08eb7d9cf61953
Lucas Leão <ilolt.llol@gmail.com>
2021-02-01 12:35:13 -0400

    Update plot_forecast.py

--- plot_forecast.py
+++ plot_forecast.py
@@ -1,5 +1,5 @@
-def plot_forecast(train_data, test_data, model):
+def plot_forecast(train_data, test_data, model, figsize=(12,8)):
     %matplotlib inline
-    train_data.plot(legend=True, label = 'TRAIN', figsize=(12,8))
-    test_data.plot(legend=True, label = 'TEST', figsize=(12,8))
-    model.plot(legend=True, label = 'PREDICT', figsize=(12,8))
+    train_data.plot(legend=True, label = 'TRAIN', figsize=figsize)
+    test_data.plot(legend=True, label = 'TEST', figsize=figsize)
+    model.plot(legend=True, label = 'PREDICT', figsize=figsize)
