4710814c9110ee0973086d30dd7b15fc0366c496
Lucas Leão <ilolt.llol@gmail.com>
2021-02-03 18:28:49 -0400

    Update plot_forecast.py

--- plot_forecast.py
+++ plot_forecast.py
@@ -2,4 +2,7 @@ def plot_forecast(training_set, test_set, model=None, figsize=(12,8), interval=N
     %matplotlib inline
     training_set.plot(legend=True, label = 'TRAIN', figsize=figsize)
     test_set.plot(legend=True, label = 'TEST', figsize=figsize)
-    model.plot(legend=True, label = 'PREDICT', figsize=figsize, xlim=interval)
+    try:
+        model.plot(legend=True, label = 'PREDICT', figsize=figsize, xlim=interval)
+    except AttributeError:
+        pass
