eac9ba503474932670dba0950c21994f09717f62
Lucas Leão <ilolt.llol@gmail.com>
2021-02-04 08:18:51 -0400

    Update seasonal_decompose.py

--- seasonal_decompose.py
+++ seasonal_decompose.py
@@ -6,5 +6,5 @@ def seasonal_decompose(data, model, figsize=(15,8), period=None):
     fig, (ax0, ax1,ax2,ax3) = plt.subplots(4,1, figsize=figsize)
     s_dec.observed.plot(ax=ax0, grid=True, legend=True)
     s_dec.trend.plot(ax=ax1, grid=True, legend=True)
-    s_dec.resid.plot(ax=ax2, grid=True, legend=True)
-    s_dec.seasonal.plot(ax=ax3, grid=True, legend=True)
+    s_dec.seasonal.plot(ax=ax2, grid=True, legend=True)
+    s_dec.resid.plot(ax=ax3, grid=True, legend=True)
