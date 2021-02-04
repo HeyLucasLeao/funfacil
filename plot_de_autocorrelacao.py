0fa9e9c625c04c77ba5367c569322de45b5b3e9d
Lucas Leão <ilolt.llol@gmail.com>
2021-02-04 08:32:23 -0400

    Update plot_de_autocorrelacao.py

--- plot_de_autocorrelacao.py
+++ plot_de_autocorrelacao.py
@@ -1,4 +1,4 @@
-def plot_de_autocorrelacao(data, title='Correlação de Dados', figsize=(12,6), lags = 40):
+def plot_de_autocorrelacao(data, title='Correlação de Dados', figsize=(12,6), lags = 40, zero = False):
     from pandas.plotting import autocorrelation_plot
     from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
     import matplotlib.pyplot as plt    
@@ -6,6 +6,6 @@ def plot_de_autocorrelacao(data, title='Correlação de Dados', figsize=(12,6), 
     ax1.suptitle(title, fontsize=18, x= 0.26, y= 0.95)
     autocorrelation_plot(data)
     
-    plot_acf(data, lags = lags).show()
-    plot_pacf(data, lags = lags).show()
+    plot_acf(data, lags = lags, zero = zero).show()
+    plot_pacf(data, lags = lags, zero = zero).show()
     ax1.show()
