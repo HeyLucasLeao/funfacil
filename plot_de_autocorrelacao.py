cdd424b8f342f78775e1f775ce093c5d3968c4d2
Lucas Leão <ilolt.llol@gmail.com>
2021-02-04 09:06:34 -0400

    Update plot_de_autocorrelacao.py

--- plot_de_autocorrelacao.py
+++ plot_de_autocorrelacao.py
@@ -1,10 +1,10 @@
-def plot_de_autocorrelacao(data, figsize=(12,6), lags = 40, zero = False, title=""):
+def plot_de_autocorrelacao(data, figsize=(12,6), lags = 40, zero = False, title="", method= 'ywadjusted'):
     from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
     import matplotlib.pyplot as plt    
     
     plot_acf(data, lags = lags, zero = zero)
     plt.title("Autocorrelação" + " "+ title)
     plt.show()
-    plot_pacf(data, lags = lags, zero = zero)
+    plot_pacf(data, lags = lags, zero = zero, method= method)
     plt.title("Autocorrelação Parcial" + " "+ title)
     plt.show()
