bb0daa0d6063e584bfc4fdf97e79a2cf7a15f0e7
Lucas Leão <ilolt.llol@gmail.com>
2021-02-03 20:22:53 -0400

    Update adfuller.py

--- adfuller.py
+++ adfuller.py
@@ -4,6 +4,8 @@ def adfuller(data):
     result = adfuller(X)
     print('ADF Statistic: %f' % result[0])
     print('p-value: %f' % result[1])
+    print(f'Lags: {result[2]}')
+    print(f'Periods: {result[3]}')
     print('Critical Values:')
     for key, value in result[4].items():
         print('\t%s: %.3f' % (key, value))
