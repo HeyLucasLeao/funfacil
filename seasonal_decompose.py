6b89e90e5c90960188ab139220b40a60aad26035
Lucas Leão <ilolt.llol@gmail.com>
2021-02-04 08:09:30 -0400

    Create seasonal_decompose.py

---
+++ seasonal_decompose.py
@@ -0,0 +1,10 @@
+def seasonal_decompose(data, model, figsize=(15,8), period=None):
+    from statsmodels.tsa.seasonal import seasonal_decompose
+    import matplotlib.pyplot as plt 
+    
+    seasonal_decompose(data, model = model, period=period)
+    fig, (ax0, ax1,ax2,ax3) = plt.subplots(4,1, figsize=figsize)
+    s_dec.observed.plot(ax=ax0, grid=True, legend=True)
+    s_dec.trend.plot(ax=ax1, grid=True, legend=True)
+    s_dec.resid.plot(ax=ax2, grid=True, legend=True)
+    s_dec.seasonal.plot(ax=ax3, grid=True, legend=True)
