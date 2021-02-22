f57a09ade0563d30611d2c3bbf55458ca839f16f
Lucas Leão <ilolt.llol@gmail.com>
2021-02-22 17:15:28 -0400

    Delete comparar_modelo.py

--- comparar_modelo.py
+++
@@ -1,8 +0,0 @@
-def comparar_modelo(test_data, model):
-    from sklearn.metrics import mean_squared_error, mean_absolute_error
-    
-    return f"""
-        Desvio Padrão: {test_data.std().round(2)}
-        Erro Médio Absoluto: {mean_absolute_error(test_data, model).round(2)}
-        Erro Médio Quadrático ao Quadrado: {np.sqrt(mean_squared_error(test_data, model)).round(2)}
-        """
