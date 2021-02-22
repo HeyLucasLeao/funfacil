56fc7041872dedcfe316730f55d0ef563eaf9665
Lucas Leão <ilolt.llol@gmail.com>
2021-02-22 17:18:24 -0400

    Delete train_test_split_AR.py

--- train_test_split_AR.py
+++
@@ -1,6 +0,0 @@
-def train_test_split_AR(data, percentage_to_test=0.33):
-    divide = 1 - percentage_to_test
-    train_size = int(len(data) * divide)
-    training_set = data[:train_size + 1]
-    test_set = data[train_size:]
-    return (training_set, test_set)
