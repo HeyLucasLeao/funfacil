023d0afbfd54c04ec7010521cfe0fce6d94f60e1
Lucas Leão <ilolt.llol@gmail.com>
2021-02-22 17:21:10 -0400

    Delete plot_forecast.py

--- plot_forecast.py
+++
@@ -1,8 +0,0 @@
-def plot_forecast(training_set, test_set, model=None, figsize=(12,8), interval=None):
-    %matplotlib inline
-    training_set.plot(legend=True, label = 'TRAIN', figsize=figsize)
-    test_set.plot(legend=True, label = 'TEST', figsize=figsize)
-    try:
-        model.plot(legend=True, label = 'PREDICT', figsize=figsize, xlim=interval)
-    except AttributeError:
-        pass
