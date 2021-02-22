90c4459792aef49077917c3cb7a887d3e424181a
Lucas Leão <ilolt.llol@gmail.com>
2021-02-22 17:35:29 -0400

    Update grid_search_ts_algorithms.py

--- grid_search_ts_algorithms.py
+++ grid_search_ts_algorithms.py
@@ -1,9 +1,15 @@
-list_of_smape = []
+import numpy as np
+from sktime.forecasting.compose import ReducedForecaster
 
-for i in range(1, 100):
-    regressor = KNeighborsRegressor(metric='manhattan', n_neighbors = i)
-    reduced_reg = ReducedForecaster(scitype ='regressor',regressor = regressor, window_length= 7)
-    reduced_reg.fit(train_scaled)
-    y_pred = reduced_reg.predict(fh)
-    list_of_smape.append([smape_loss(test_scaled, y_pred), i])
-    
+def grid_search_ts_algorithms(y_train, y_test, algorithms=[], feature_range=(1, 100), window_length = 7):
+    fh = np.arange(1, len(y_test) + 1)
+    list_of_algorithms = algorithms
+    list_of_smape = []
+
+    for i in range(feature_range):
+        for algorithm in list_of_algorithms:
+            reduced_reg = ReducedForecaster(scitype ='regressor',regressor = algorithm, window_length= window_length)
+            reduced_reg.fit(y_train)
+            y_pred = reduced_reg.predict(fh)
+            list_of_smape.append([smape_loss(y_test, y_pred), i])
+    return min(list_of_smape)
