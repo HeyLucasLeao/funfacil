acfc5128e9ae583ca20f27a9f8e634e566aac29c
Lucas Leão <ilolt.llol@gmail.com>
2021-02-22 17:55:53 -0400

    Update cleaning_missing_data.py

--- cleaning_missing_data.py
+++ cleaning_missing_data.py
@@ -1,8 +1,8 @@
 from sklearn.impute import SimpleImputer
 import numpy as np
 
-def cleaning_missing_data(dataset, , strategy='median', missing_values=np.nan):
+def cleaning_missing_data(data_frame, , strategy='median', missing_values=np.nan):
     imputer = SimpleImputer(missing_values= missing_values, 
     strategy=strategy)
-    dados_imputados = imputer.fit_transform(dataset)
+    dados_imputados = imputer.fit_transform(data_frame)
     return dados_imputados
