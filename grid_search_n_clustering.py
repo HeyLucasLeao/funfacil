1a52678cafe19053d024238a5bf06e843151599f
Lucas Leão <ilolt.llol@gmail.com>
2021-02-23 12:15:08 -0400

    Update grid_search_n_clustering.py

--- grid_search_n_clustering.py
+++ grid_search_n_clustering.py
@@ -12,12 +12,25 @@ def clustering_algorithm(n_clusters, dataset):
     calinski = metrics.calinski_harabasz_score(dataset, labels)
     return [s, db, calinski]
 
-def grid_search_n_clustering(dataset):
+def grid_search_n_clustering(dataset, start=2, end=30):
     result_grid_search = []
-    for i in range(2, 30):
+    for i in range(start, end):
         result_grid_search.append([i, clustering_algorithm(i, dataset)])
-    result = result_grid_search[0][1]
-    for x in result_grid_search:
-        if x[1] > result:
-            result = x
-    return result
+
+    r1 = result_grid_search[0]
+    r2 = result_grid_search[0]
+    r3 = result_grid_search[0]
+
+    for i in range(len(result_grid_search)):
+        if result_grid_search[i][1][0] > r1[1][0]:
+            r1 = result_grid_search[i]
+
+        if result_grid_search[i][1][1] < r2[1][1]:
+            r2 = result_grid_search[i]
+
+        if result_grid_search[i][1][2] > r3[1][2]:
+            r3 = result_grid_search[i]
+
+    return f"""Silhouette: N.º de Clusters: {r1[0]} -------- {r1[1]}, 
+    Davies Bouldin: N.º de Clusters: {r2[0]} -------- {r2[1]}, 
+    Calinski Harabrasz: N.º de Clusters: {r3[0]} -------- {r3[1]}"""
