d9ae7f239be64234e534fbef9828b23c5e7c18e2
Lucas Leão <ilolt.llol@gmail.com>
2021-03-07 12:04:51 -0400

    Create treinar_modelo_normal.py

---
+++ treinar_modelo_normal.py
@@ -0,0 +1,37 @@
+def treinar_modelo(params):
+    SEED = 4
+
+    learning_rate = params[0]
+    num_leaves = params[1]
+    min_child_samples = params[2]
+    subsample = params[3]
+    colsample_bytree = params[4]
+    n_estimators = params[5]
+
+    print(params, '\n')
+    
+    mdl = LGBMRegressor(random_state=SEED,
+    learning_rate = learning_rate, 
+    num_leaves = num_leaves, 
+    min_child_samples = min_child_samples, 
+    subsample = subsample,
+    colsample_bytree = colsample_bytree,
+    subsample_freq=1,
+    n_estimators=n_estimators)
+    mdl.fit(X_train, y_train)
+    y_pred = mdl.predict(X_test)
+
+    return mean_absolute_percentage_error(y_test, y_pred)
+
+space = [(1e-3, 1e-1, 'log-uniform'), #learning rate
+(2, 128), #num_leaves
+(1, 100), #min_child_samples
+(0.05, 1.0), #subsample
+(0.1, 1.0), #colsample_bytree
+(100, 1000)] #n_estimators
+
+result = dummy_minimize(treinar_modelo, 
+space,
+random_state=SEED, 
+verbose=1,
+n_calls = 30)
