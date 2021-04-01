d14668a2f735063ad2a4a99962642d42174729ff
Lucas Leão <ilolt.llol@gmail.com>
2021-04-01 16:33:27 -0400

    Create check_gpu.py

---
+++ check_gpu.py
@@ -0,0 +1,14 @@
+from numba import cuda
+import tensorflow as tf
+
+physical_device = tf.config.experimental.list_physical_devices('GPU')
+try:
+  tf.config.experimental.set_memory_growth(physical_devices[0], True)
+  assert tf.config.experimental.get_memory_growth(physical_devices[0])
+except:
+  print('Invalid device or cannot modify virtual devices once initialized.')
+  pass
+  
+# helps after out of memory errors
+device = cuda.get_current_device()
+device.reset()
