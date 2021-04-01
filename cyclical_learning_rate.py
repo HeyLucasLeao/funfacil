2c0b5edf6dade82b12e718e478c28d3660347185
Lucas Leão <ilolt.llol@gmail.com>
2021-04-01 13:32:13 -0400

    Update cyclical_learning_rate.py

--- cyclical_learning_rate.py
+++ cyclical_learning_rate.py
@@ -1,17 +1,13 @@
-from tensorflow_addons.optimizers import CyclicalLearningRate
+from tensorflow_addons.optimizers import Triangular2CyclicalLearningRate
 
-def cyclical_learning_rate_schedule(lr_max, n_items, bs=64):
-  maximal_learning_rate=lr_max
-  initial_learning_rate=maximal_learning_rate/10
-  epochs_within_each_step = 2
-  iterations_in_epoch = n_items//bs
-  step_size = iterations_in_epoch * epochs_within_each_step
-  cycles = step_size * 2
-  training_steps= cycles * 3
+def cyclical_learning_rate_schedule(lr_max, n_items, bs=64, step_size=8):
+    maximal_learning_rate=lr_max
+    initial_learning_rate=maximal_learning_rate/10
+    iterations_in_epoch = n_items//bs
+    step_size = iterations_in_epoch * step_size
 
-  cyclical_learning_rate = CyclicalLearningRate(initial_learning_rate=initial_learning_rate, 
-  maximal_learning_rate=maximal_learning_rate, 
-  step_size=step_size, 
-  scale_fn=lambda x: 1 / (2.0 ** (x - 1))) #triangular2
+    cyclical_learning_rate = Triangular2CyclicalLearningRate(initial_learning_rate=initial_learning_rate, 
+    maximal_learning_rate=maximal_learning_rate, 
+    step_size=step_size)
 
-  return cyclical_learning_rate
+    return cyclical_learning_rate
