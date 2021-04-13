08b11bc6846addb74764996ba76a249eded327af
Lucas Leão <ilolt.llol@gmail.com>
2021-04-13 09:19:54 -0400

    Update check_gpu_on_tensorflow.py

--- check_gpu_on_tensorflow.py
+++ check_gpu_on_tensorflow.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 physical_device = tf.config.experimental.list_physical_devices('GPU')
 try:
-  tf.config.experimental.set_memory_growth(physical_devices[0], True)
-  assert tf.config.experimental.get_memory_growth(physical_devices[0])
+  tf.config.experimental.set_memory_growth(physical_device[0], True)
+  assert tf.config.experimental.get_memory_growth(physical_device[0])
 except:
   print('Invalid device or cannot modify virtual devices once initialized.')
   pass
