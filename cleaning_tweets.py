f2e22072cf8f06111c3dd75b8ba9797fc16064a0
Lucas Leão <ilolt.llol@gmail.com>
2021-04-30 06:58:06 -0400

    Create cleaning_tweets.py

---
+++ cleaning_tweets.py
@@ -0,0 +1,10 @@
+def cleaning_tweets(data, language='pt'):
+    dataset = data.copy()
+    dataset = dataset[dataset['language'] == language]
+    dataset.drop_duplicates('tweet', inplace=True)
+    dataset.drop_duplicates('username', inplace=True)
+    dataset['hashtags'] = [", ".join(y for y in re.findall('\B#\w\w+', x)) for x in dataset['tweet']]
+    dataset = dataset[['date','tweet','hashtags']]
+    dataset.set_index('date', inplace=True)
+
+    return dataset
