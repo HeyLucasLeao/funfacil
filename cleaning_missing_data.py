6abb2839742677929ce9814e07af903db68fb4e8
Lucas Leão <ilolt.llol@gmail.com>
2021-04-30 06:56:25 -0400

    Delete cleaning_missing_data.py

--- cleaning_missing_data.py
+++
@@ -1,8 +0,0 @@
-from sklearn.impute import SimpleImputer
-import numpy as np
-
-def cleaning_missing_data(data_frame, , strategy='median', missing_values=np.nan):
-    imputer = SimpleImputer(missing_values= missing_values, 
-    strategy=strategy)
-    dados_imputados = imputer.fit_transform(data_frame)
-    return dados_imputados
